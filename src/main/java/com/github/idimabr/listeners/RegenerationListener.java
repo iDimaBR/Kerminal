3c4fb51f2c85d607ed46fe0b2fd5590cce8c9313
Raphael Alexei <raphaeeelbr@gmail.com>
2022-06-05 19:28:19 -0300

    Added system of regeneration life

---
+++ src/main/java/com/github/idimabr/listeners/RegenerationListener.java
@@ -0,0 +1,19 @@
+package com.github.idimabr.listeners;
+
+import com.github.idimabr.Kerminal;
+import lombok.AllArgsConstructor;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityRegainHealthEvent;
+
+@AllArgsConstructor
+public class RegenerationListener implements Listener {
+
+    private Kerminal plugin;
+
+    @EventHandler
+    public void InventoryClick(EntityRegainHealthEvent e) {
+        if(!e.getRegainReason().equals(EntityRegainHealthEvent.RegainReason.CUSTOM))
+            e.setCancelled(true);
+    }
+}
